f1cc806edd8fc416fc5ca273a444c510b8b2316f
aanthr0 <anthromadayt@gmail.com>
2021-07-01 09:27:38 +0200

    Added config file

---
+++ src/config-example.js
@@ -0,0 +1,15 @@
+module.exports = {
+    owners: ["OWNER", "IDs", "HERE"],
+    uri: "MONGODB+SRV://URI:HERE",
+    token: "BOT-TOKEN-HERE",
+    prefix: "PREFIX-HERE",
+    color: {
+        positive: "GREEN",
+        negative: "RED"
+    },
+    emoji: {
+        positive: ":green_circle:",
+        negative: ":red_circle:"
+    },
+    footer: "EMBED-FOOTER"
+}
