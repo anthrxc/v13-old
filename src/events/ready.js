3aa5d69e5c542f8a5f012063e616f47e6a8e1d98
Ivan <anthromadayt@gmail.com>
2021-08-09 09:48:34 +0200

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
     console.clear();
-    console.log(`Pyxis v${require("../../package.json").version} is online.\nMade with <3 by aanthr0, inc.\n----------------------------------------`);
+    console.log(`djs-template v${require("../../package.json").version} is online.\nMade with <3 by aanthr0, inc.\n----------------------------------------`);
     setTimeout(
         () => {
             console.clear();
